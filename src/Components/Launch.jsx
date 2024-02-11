afa3431c2baff27e31972dbd402d19ab6f48098c
Keshav Soni <keshavvsonii01@gmail.com>
2024-02-11 16:04:13 +0530

    Update Media

--- src/Components/Launch.jsx
+++ src/Components/Launch.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import "../css/launch.css";
 
+
+
 function Launch() {
   return (
     <>
-    <div className='launch-container'>
-      <video className='launch-video' id='desktopvideo' src="./images/vid3.mp4" loop type="video/mp4" autoPlay muted></video>
+    <div className='launch-container'  id='desktopVideo'>
+      <video className='launch-video' id='desktopVideo' src="./images/vid3.mp4" loop type="video/mp4" autoPlay muted></video>
     </div>
-     <div className='launch-container'>
-     <video className='launch-video' id='mobilevideo' src="./images/vid33.mp4" loop type="video/mp4" autoPlay muted></video>
+     <div className='launch-container'  id='mobileVideo'>
+     <video className='launch-video' id='mobileVideo' src="./images/vid33.mp4" loop type="video/mp4" autoPlay muted></video>
    </div>
    </>
   );
