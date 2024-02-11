96fccafccf67e1a4950a8e08b923ac0d29b2f84f
Keshav Soni <keshavvsonii01@gmail.com>
2024-02-11 15:30:50 +0530

    Add Compressed Video

--- src/Components/Launch.jsx
+++ src/Components/Launch.jsx
@@ -3,9 +3,14 @@ import "../css/launch.css";
 
 function Launch() {
   return (
+    <>
     <div className='launch-container'>
-      <video className='launch-video' src="./images/vid3.mp4" loop type="video/mp4" autoPlay muted></video>
+      <video className='launch-video' id='desktopvideo' src="./images/vid3.mp4" loop type="video/mp4" autoPlay muted></video>
     </div>
+     <div className='launch-container'>
+     <video className='launch-video' id='mobilevideo' src="./images/vid33.mp4" loop type="video/mp4" autoPlay muted></video>
+   </div>
+   </>
   );
 }
 
