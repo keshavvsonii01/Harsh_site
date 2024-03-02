27700e80a0de6f3673d39c71ff305ed388c47f45
Keshav Soni <keshavvsonii01@gmail.com>
2024-03-02 15:32:22 +0530

    Nav Atleast Working

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,54 +4,60 @@ import "../css/header.css";
 export const Header = () => {
   return (
     <>
-      <div className="container">
-        <nav class="navbar navbar-expand-lg">
-          <div class="container-fluid">
-            <div className="Header">
-              <div className="logo">
-                <a class="navbar-brand" href="#">
-                  <img className="header-logo" src="./images/2.png" />
-                </a>
-              </div>
+      <header>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+              <div className="Header">
+                <div className="logo">
+                  <a className="navbar-brand" href="">
+                    <img className="header-logo" src="./images/2.png" />
+                  </a>
+                </div>
 
-              <div className="navigate">
-                <button
-                  class="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNav"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                  <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">
-                      Home
-                    </a>
-                    <a class="nav-link" href="#services">
-                      Our Services
-                    </a>
-                    <a class="nav-link" href="#team">
-                      Our Team
-                    </a>
-                    <a class="nav-link" href="#work">
-                      Our Work
-                    </a>
-                    <a class="nav-link" href="#blog">
-                      Blog
-                    </a>
-                  </div>
-                  <button className="header_btn">
-                    <span href="">Request a Free Quote</span>
+                <div className="navigate">
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="navbar-toggler-icon"></span>
                   </button>
+                  <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="navbar-nav">
+                      <a
+                        className="nav-link active"
+                        aria-current="page"
+                        href="#"
+                      >
+                        Home
+                      </a>
+                      <a className="nav-link" href="#services">
+                        Our Services
+                      </a>
+                      <a className="nav-link" href="#team">
+                        Our Team
+                      </a>
+                      <a className="nav-link" href="#work">
+                        Our Work
+                      </a>
+                      <a className="nav-link" href="#blog">
+                        Blog
+                      </a>
+                    </div>
+                    <button className="header_btn">
+                      <span href="">Request a Free Quote</span>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </nav>
-      </div>
+          </nav>
+        </div>
+      </header>
     </>
   );
 };
